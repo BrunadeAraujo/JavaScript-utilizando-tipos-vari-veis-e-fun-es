f5faf00054a6565b908df6d438287b62e1fdd54b
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-02-27 21:15:07 -0300

    second

---
+++ tipo-boolean.js
@@ -0,0 +1,13 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if( estaAprovado === true){
+    console.log('Parabéns, Boas Festas');
+} else{
+    console.log('Está Reprovado');
+}
+if (estudante === 'Fernando'){
+    console.log(`Olá, ${estudante}`);
+}else{
+    console.log('Quem é vc?');
+}
---
+++ tipo-string.js
@@ -0,0 +1,12 @@
+const estudante = "Caroline";
+const docente = 'Ana';
+const cumprimento = "Nosso lema é 'Estudar Bastante!' ";
+const citacao = `Ju diz: "Nosso lema é 'Estudar Bastante!'"`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`A estudante se chama ${estudante}`);
+
+const senha = 'SenhaSegra123' + estudante.toUpperCase();
+console.log(senha);
